59f71afab302b4e80a125cc31920c7f1d1693160
CharlesCarley <snailrose@gmail.com>
2020-05-17 19:09:39 -0600

    Update

---
+++ Source/Test.asm
@@ -0,0 +1,17 @@
+.data
+
+
+.text
+main:
+    mov   x0, 17654876
+    mov   x1, 1
+    mov   x2, 2
+    mov   x3, 3
+    mov   x4, 4
+    mov   x5, 5
+    mov   x6, 6
+    mov   x7, 7
+    mov   x8, 8
+    mov   x9, 9
+    ret
+
