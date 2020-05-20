725f8cf3e9a4c4a2292afaf520245c97bca30ccd
CharlesCarley <snailrose@gmail.com>
2020-05-20 05:41:58 -0600

    Update

--- Test/IfElse.asm
+++ Test/IfElse.asm
@@ -1,27 +1,16 @@
 main:
     mov x0, 0
-
 top:
-
     cmp x0, 10
     jge done
     inc x0
-
     cmp x0, 5
     jlt lt5
-
     prg 1
     jmp top
-
 lt5:
     prg 0
     jmp top
-
 done:
     mov x0, 0
     ret
-
-
-
-
-
