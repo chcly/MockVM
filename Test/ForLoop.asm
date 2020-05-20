caad252b52ae547606ed24916029e0e05dd8dc1c
CharlesCarley <snailrose@gmail.com>
2020-05-20 06:28:35 -0600

    Update

--- Test/ForLoop.asm
+++ Test/ForLoop.asm
@@ -1,13 +1,12 @@
 main:
-    mov  x0, 0
-    mov  x1, 10
- top:
-    cmp   x0, x1
-    jge   done
-    inc   x0
-    prg   x0
-    jmp   top
+mov x0, 0
+mov x1, 10
+top:
+cmp x0, x1
+jge done
+inc x0
+prg x0
+jmp  top
 done:
-    mov  x0, 0
-    ret
-
+mov  x0, 0
+ret
