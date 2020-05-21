b46dea345318fa6559bd943f18140c109d856152
CharlesCarley <snailrose@gmail.com>
2020-05-21 09:32:24 -0600

    Added a label test

---
+++ Test/Label.asm
@@ -0,0 +1,13 @@
+__abcdefghijklmnopqrstuvwzyz_0123456789__:
+    mov x0, 0
+    ret
+__ABCDEFGHIJKLMNOPQRSTUVWZYZ_0123456789__:
+    mov x0, 0
+    ret
+
+main:
+    call __abcdefghijklmnopqrstuvwzyz_0123456789__
+    call __ABCDEFGHIJKLMNOPQRSTUVWZYZ_0123456789__
+    ret
+
+
