a758c6f617a6d76a65301af0f48f674f34868955
CharlesCarley <snailrose@gmail.com>
2020-05-21 09:58:52 -0600

    Extended the test to include a second operand where applicable.

--- Test/Keywords.asm
+++ Test/Keywords.asm
@@ -9,14 +9,21 @@ main:
     jgt  ops
     call ops
     div  x0, x1
+    div  x0, 123
     shr  x0, x1
+    shr  x0, 123
     shl  x0, x1
+    shl  x0, 123
     inc  x0
     dec  x1 
     mul  x0, x1
+    mul  x0, 123
     add  x0, x1
+    add  x0, 123
     sub  x0, x1
+    sub  x0, 123
     prg  x0
+    prg  123
     prgi
 ops:
     mov x0, 0   
