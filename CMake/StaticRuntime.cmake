7c4b88db27806f9c6dafc5d91bd4866f903feee5
CharlesCarley <snailrose@gmail.com>
2020-05-21 16:04:21 -0600

    Removed the code to disable warning messages.

--- CMake/StaticRuntime.cmake
+++ CMake/StaticRuntime.cmake
@@ -20,33 +20,13 @@ macro(set_static_runtime)
 			    string(REGEX REPLACE "/MDd" "/MTd" ${flag_var} "${${flag_var}}")
 		    endif(${flag_var} MATCHES "/MDd")
 	    endforeach(flag_var)
-
-
-
-	    foreach(flag_var    CMAKE_CXX_FLAGS 
-	                        CMAKE_CXX_FLAGS_DEBUG 
-	                        CMAKE_CXX_FLAGS_RELEASE 
-	                        CMAKE_CXX_FLAGS_MINSIZEREL 
-	                        CMAKE_CXX_FLAGS_RELWITHDEBINFO
-	                        CMAKE_C_FLAGS_DEBUG
-	                        CMAKE_C_FLAGS_RELEASE
-	                        CMAKE_C_FLAGS_MINSIZEREL 
-	                        CMAKE_C_FLAGS_RELWITHDEBINFO
-	                        )
-            set(${flag_var} "${${flag_var}} -D_CRT_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE")
-	    endforeach(flag_var)
-	    
-	    
     endif(MSVC)
-
 endmacro(set_static_runtime)
-
 
 
 macro(disable_crt_warnings)
 
     if(MSVC)
-
 	    foreach(flag_var    CMAKE_CXX_FLAGS 
 	                        CMAKE_CXX_FLAGS_DEBUG 
 	                        CMAKE_CXX_FLAGS_RELEASE 
@@ -59,8 +39,6 @@ macro(disable_crt_warnings)
 	                        )
             set(${flag_var} "${${flag_var}} -D_CRT_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_DEPRECATE -D_CRT_NONSTDC_NO_DEPRECATE")
 	    endforeach(flag_var)
-	    
-	    
     endif(MSVC)
 
 endmacro(disable_crt_warnings)
