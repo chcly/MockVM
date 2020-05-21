6e223c5abc9dd264c4a5dae6043cc52c74ecfbd3
CharlesCarley <snailrose@gmail.com>
2020-05-21 16:16:33 -0600

    Updated the workflow file

--- Test/Catch2.h
+++ Test/Catch2.h
@@ -1,5 +1,7 @@
-#ifndef __MACRO_H_
-#define __MACRO_H_
+#ifndef _Catch2_h_
+#define _Catch2_h_
+
+
 #include "catch/catch.hpp"
 
 #define EXPECT_EQ(A, B) REQUIRE((A) == (B))
@@ -8,9 +10,9 @@
 #define EXPECT_GT(A, B) REQUIRE((A) > (B))
 #define EXPECT_GE(A, B) REQUIRE((A) >= (B))
 #define EXPECT_LE(A, B) REQUIRE((A) <= (B))
-#define EXPECT_TRUE(A) REQUIRE((A) == true)
+#define EXPECT_TRUE(A)  REQUIRE((A) == true)
 #define EXPECT_FALSE(A) REQUIRE((A) == false)
 
 const bool DEBUG = true;
 
-#endif
+#endif  //_Catch2_h_
