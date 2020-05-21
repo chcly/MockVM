16d0a58bc9737eb2af477128897e71969abd9390
CharlesCarley <snailrose@gmail.com>
2020-05-21 09:42:59 -0600

    Added tests for line endings.

--- Test/Label.asm
+++ Test/Label.asm
@@ -4,10 +4,7 @@ __abcdefghijklmnopqrstuvwzyz_0123456789__:
 __ABCDEFGHIJKLMNOPQRSTUVWZYZ_0123456789__:
     mov x0, 0
     ret
-
 main:
     call __abcdefghijklmnopqrstuvwzyz_0123456789__
     call __ABCDEFGHIJKLMNOPQRSTUVWZYZ_0123456789__
     ret
-
-
