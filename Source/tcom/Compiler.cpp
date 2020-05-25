9124fcef5e989b8515f9edbe332157393436b30a
CharlesCarley <snailrose@gmail.com>
2020-05-25 16:48:47 -0600

    Update

--- Source/tcom/Compiler.cpp
+++ Source/tcom/Compiler.cpp
@@ -96,6 +96,8 @@ int main(int argc, char **argv)
         return PS_ERROR;
     }
 
+    if (find(ctx.modules.begin(), ctx.modules.end(), "std") == ctx.modules.end())
+        ctx.modules.push_back("std");
 
     FindExecutableDirectory(ctx.modulePath);
 
