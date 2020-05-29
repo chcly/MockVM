11f2c7cffdb24d686b2e48027761d39b1ff5576e
CharlesCarley <snailrose@gmail.com>
2020-05-28 19:02:46 -0600

    Update

---
+++ Test/Scan/Scan5.asm
@@ -0,0 +1,25 @@
+; ----------------------------------------------------
+                .data
+; ----------------------------------------------------
+message1: .asciz "Hello World #1"
+message2: .asciz "Hello World #2"
+message3: .asciz "Hello World #3"
+message4: .asciz "Hello World #4"
+message5: .asciz "Hello World #5"
+; ----------------------------------------------------
+                .text
+; ----------------------------------------------------
+main:
+    adrp  x0, message1
+    bl    puts
+    adrp  x0, message2
+    bl    puts
+    adrp  x0, message3
+    bl    puts
+    adrp  x0, message4
+    bl    puts
+    adrp  x0, message5
+    bl    puts
+    prgi
+    mov   x0, 0
+    ret
