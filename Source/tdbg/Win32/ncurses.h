4b8e6a75a49201ea80560fa85574aade3b8cad68
CharlesCarley <snailrose@gmail.com>
2020-06-11 15:59:05 -0600

    Added stub files. Added a color palette for use with colortool. https://github.com/Microsoft/Terminal/tree/master/src/tools/ColorTool

---
+++ Source/tdbg/Win32/ncurses.h
@@ -0,0 +1,27 @@
+// Stub file for intellisense
+
+#define KEY_DOWN 13
+#define KEY_ENTER 13
+
+typedef void* WINDOW;
+extern WINDOW* stdscr;
+
+
+#define COLOR_PAIR(c) c
+
+WINDOW* initscr(void);
+int     endwin(void);
+int     delch(void);
+void    move(int x, int y);
+int     attron(int attrs);
+int     attroff(int attrs);
+int     insch(char ch);
+int     curs_set(int visibility);
+int     refresh(void);
+int     has_colors(void);
+int     start_color(void);
+int     noecho(void);
+int     keypad(WINDOW* win, bool bf);
+void    use_default_colors(void);
+void    getmaxyx(WINDOW*, int r, int c);
+void    init_pair(int, int, int);
---
+++ Source/tdbg/Win32/unistd.h
@@ -0,0 +1,11 @@
+// Stub file for intellisense
+
+
+int getch();
+int pipe(int *pd);
+int dup(int);
+int dup2(int, int);
+void close(int);
+
+void usleep(int);
+void ungetch(int);
